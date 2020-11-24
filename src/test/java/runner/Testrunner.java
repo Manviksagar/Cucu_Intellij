13ccb1913d7ed2b94c807497b1227af56e50a35f
Manviksagar <aallu.vidyasagar@gmail.com>
2020-11-24 19:11:00 +0530

    Working all reports extent and Cucumber JVM

--- src/test/java/runner/Testrunner.java
+++ src/test/java/runner/Testrunner.java
@@ -1,17 +1,35 @@
 package runner;
 
+import com.cucumber.listener.Reporter;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+
+import java.io.File;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/Featuers"
         ,glue= {"com.stepdefnition"},
-        plugin = { "pretty" ,"html:target/cucumber-reports", "json:target/cucumber-reports/Cucumber.json"},
+        plugin = { "pretty" ,"html:target/cucumber-reports", "json:target/cucumber-reports/Cucumber.json",
+                "com.cucumber.listener.ExtentCucumberFormatter:target/html/ExtentReport.html"},
+
         monochrome = true
 )
 
 public class Testrunner {
+
+    @AfterClass
+    public static void setup()
+    {
+        Reporter.loadXMLConfig(new File("src/test/resources/extent-config.xml"));
+//Reporter.setSystemInfo("Test User", System.getProperty("user.name"));
+        Reporter.setSystemInfo("User Name", "AJ");
+        Reporter.setSystemInfo("Application Name", "Test App ");
+        Reporter.setSystemInfo("Operating System Type", System.getProperty("os.name").toString());
+        Reporter.setSystemInfo("Environment", "Production");
+        Reporter.setTestRunnerOutput("Test Execution Cucumber Report");
+    }
 
 }
